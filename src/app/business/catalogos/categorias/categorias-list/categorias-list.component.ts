add52fb65b7acca6b4d07246289312151d4ab510
RodolfoBaume <robalaz@hotmail.com>
2024-10-01 09:34:43 -0600

    Restaurar archivos categorias

--- src/app/business/catalogos/categorias/categorias-list/categorias-list.component.ts
+++ src/app/business/catalogos/categorias/categorias-list/categorias-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CategoriaService } from '../../../../core/services/categoria.service';
+import { Categoria } from '../../../../core/models/categoria.modelo';
 
 @Component({
   selector: 'app-categorias-list',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './categorias-list.component.css'
 })
 export class CategoriasListComponent {
+  categorias: Categoria[] = [];
 
+  constructor(private categoriaService: CategoriaService
+  ) { }
+
+  ngOnInit(): void {
+    this.categoriaService.getCategorias().subscribe((data: Categoria[]) => {
+      this.categorias = data;
+    });
+  }
+
+  onDelete(id: number): void {
+    if (confirm('¿Estás seguro de que deseas eliminar este Categoria?')) {
+      this.categoriaService.deleteCategoria(id).subscribe(
+        (response) => {
+          console.log('Categoria eliminada exitosamente');
+          // Filtrar la lista de proveedores en el componente
+          this.categorias = this.categorias.filter(categoria => categoria.idCategoria !== id);
+
+        },
+        (error) => {
+          console.error('Error al eliminar la Categoria:', error);
+        }
+      );
+    }
+  }
 }
